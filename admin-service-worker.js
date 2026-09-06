cbaa36ae3606a73b11d21c76d3b6edf0b3c8faa2
codegeass25 <angelroy.longakit@deped.gov.ph>
2026-09-07 04:23:31 +0800

    Add files via upload

--- admin-service-worker.js
+++ admin-service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='harborlight-admin-v4-9';
+const CACHE='harborlight-admin-v5-premium-1';
 const SHELL=['./admin.html','./admin-manifest.json','./config.js','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('harborlight-admin-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
